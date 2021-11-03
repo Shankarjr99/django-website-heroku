7031fa06416e996beef0f243d31171d16dc154e4
Shankarjr99 <66768490+Shankarjr99@users.noreply.github.com>
2021-11-03 23:15:32 +0545

    third commit2223

--- project77/settings.py
+++ project77/settings.py
@@ -148,11 +148,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)),
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR,"static")
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR,'static/image')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"static")
+]
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/image')
 MEDIA_URL='/media/'
 
 MESSAGE_TAGS = {
